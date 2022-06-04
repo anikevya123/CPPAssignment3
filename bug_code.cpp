037a43daa8ab7db2e15cc8212af4364c316e3613
aniketh vyas <93567609+anikevya123@users.noreply.github.com>
2022-06-03 21:31:19 -0500

    Add files via upload

---
+++ bug_code.cpp
@@ -0,0 +1,21 @@
+
+int main()
+{
+    int n(0);
+    cin>>n;
+    long val=factorial(n);
+    cout<<val;
+    cin.get();
+    return 0;
+}
+      
+long factorial(int n)
+{
+    long result(1);
+    while(n>0) //doesn't let n reach 0
+{
+  result*=n;
+  n--;        //decrements only after the evaluation
+}
+}
+
